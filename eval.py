48e5621cc2e10ccaa4d7d4ac12dd4714d9665f2d
taminhnd <22521292@gm.uit.edu.vn>
2025-12-10 08:17:14 +0700

    merge project that solve the conflict in 2 branch

--- eval.py
+++ eval.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 # Load reward logs
 r_base = np.load("ppo_baseline_rewards.npy")
-r_hybrid = np.load("ppo_hybrid_rewards.npy")
 r_hybrid_new = np.load("ppo_hybrid_new_rewards.npy")
 
 # Smoothing (moving average)
